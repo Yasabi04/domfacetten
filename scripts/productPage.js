468947803711d88771983bf5386f90b4487fa2f4
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2024-09-07 20:46:51 +0200

    Mehr Produkte, werden mit localStorage auch gespeichert

--- scripts/productPage.js
+++ scripts/productPage.js
@@ -14,16 +14,21 @@ function parseCSV(csv, delimiter = ',') {
     // HTML-Elemente für jedes Produkt erstellen
     function generateHTML(products) {
     return products.map(product => `
-        <article class="productCard" id="product-${product.ArtNr}">
-            <a href="singleProduct.html" data-product="${product.ArtNr}">
+        <article class="productCard" 
+            id="product-${product.ArtNr}" 
+            data-artnr="${product.ArtNr}"
+            data-preis="${product.Preis}"
+            data-bezeichnung="${product.Bezeichnung}"
+            data-foto="${product.Foto}">
+            <a href="singleProduct.html">
                 <img src="${product.Foto}" alt="Produktbild von ${product.Bezeichnung}">
             </a>
-                <abbr title="Produkt speichern">
-                    <span class = "star"><i class="fa-regular fa-star"></i></span>
-                </abbr>
-                <h2>${product.Bezeichnung}</h2>
-                <p>Preis: ${product.Preis} €</p>
-                <p>Farben: ${product.Farben}</p>
+            <abbr title="Produkt speichern">
+                <span class="star"><i class="fa-regular fa-star"></i></span>
+            </abbr>
+            <h2>${product.Bezeichnung}</h2>
+            <p>Preis: ${product.Preis} €</p>
+            <p>Farben: ${product.Farben}</p>
         </article>
     `).join('');
     }
@@ -32,12 +37,17 @@ function parseCSV(csv, delimiter = ',') {
     const csvData = `Foto,Bezeichnung,ArtNr,Preis,Farben
                 images/koelnerDom.jpg,Armband,1234,3.75,ambergreen,borealis,crystalpurple
                 images/Logo.png,Armband2,097124,4.50,black,white
-                images/Logo.png,Armband3,0911,13.99,auratiumgold,technogreen,bridgeblue`;
+                images/Logo.png,Armband3,0911,13.99,auratiumgold,technogreen,bridgeblue
+                images/mrCrabs.gif,Armband4,3994,10.75,crystalpurple
+                images/bracelet1.png,Armband5,0001,7.99,black,white
+                images/smallLogo.png,Armband6,0002,9.99,black,white
+                images/braceletWrist.png,Armband7,0003,5.99,black,white`;
 
     // Produkte parsen und HTML generieren
     const products = parseCSV(csvData, ',');
     document.getElementById('product-container').innerHTML = generateHTML(products);
 
+    /*
     document.querySelectorAll('.product').forEach(productElement => {
         productElement.addEventListener('click', (event) => {
         const productData = event.currentTarget.getAttribute('data-product');
@@ -45,20 +55,28 @@ function parseCSV(csv, delimiter = ',') {
         alert('Produkt gespeichert: ' + productData);
     });
     });
+    */
 
     document.querySelectorAll(".star").forEach(starElement => {
         starElement.addEventListener('click', (event) => {
             if(event.currentTarget.innerHTML === '<i class="fa-regular fa-star"></i>') {
                 // Ändere zu ausgefülltem Stern
                 event.currentTarget.innerHTML = '<i class="fa-solid fa-star"></i>';
-                //Hinzugefügt: Speichere die Produktdaten in localStorage
-                const productData = event.currentTarget.getAttribute('data-product');
-                localStorage.setItem('selectedProduct', productData);
-                console.log('Produkt gespeichert: ' + productData);
-                
+                // Produktinformationen direkt aus HTML Code holen
+                const productCard = event.currentTarget.closest('.productCard');
+                const productData = {
+                    artnr: productCard.getAttribute('data-artnr'),
+                    preis: productCard.getAttribute('data-preis'),
+                    bezeichnung: productCard.getAttribute('data-bezeichnung'),
+                    foto: productCard.getAttribute('data-foto')
+                };
+                console.log('Produktinformationen: ' + productData.artnr);
+                localStorage.setItem('safedProduct', JSON.stringify(productData));
             } else {
                 // Ändere zu regulärem Stern
                 event.currentTarget.innerHTML = '<i class="fa-regular fa-star"></i>';
+                console.log('Produktinformationen aus Mein Bereich gelöscht' + localStorage.getItem('safedProduct'));
+                localStorage.removeItem('safedProduct');
             }
         });
     });
