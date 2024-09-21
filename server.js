1d5e0c91dded3d75c65c259b6a6f51e4acb30a4b
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2024-09-21 16:13:30 +0200

    Neuster Stand

---
+++ server.js
@@ -0,0 +1,5 @@
+//Hier kommt die session basierte authentifizierung hin
+
+//Hier wird die Session erstellt und die Userdaten in der Session gespeichert
+//Hier wird die Session mit dem User verglichen
+//Hier wird die Session beendet
