0bd207ae6b4fb058699fee56379a94167a1c2539
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2024-12-13 23:05:09 +0100

    Ähnliche Produkte ohne Produkte fertig

--- scripts/singleProduct.js
+++ scripts/singleProduct.js
@@ -3,3 +3,8 @@
 const cargoDelivery = JSON.parse(".localStorage.getItem('cargoShip')") || [];
 
 console.log(cargoDelivery);
+
+
+// Hole das Produkt das in spaceship gespeichert wurde und zeige es auf der singleProduct.html Seite an
+const product = JSON.parse(localStorage.getItem('product')) || {};
+console.log(product);
--- server.js
+++
@@ -1,5 +0,0 @@
-//Hier kommt die session basierte authentifizierung hin
-
-//Hier wird die Session erstellt und die Userdaten in der Session gespeichert
-//Hier wird die Session mit dem User verglichen
-//Hier wird die Session beendet
